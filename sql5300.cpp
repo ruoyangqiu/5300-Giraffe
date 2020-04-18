6c6c8f28a1f4d6f292409ce36b3b42bb9d51b203
Matthew Echert <mgechert@gmail.com>
2020-04-18 14:42:53 -0700

    Read in a loop

--- sql5300.cpp
+++ sql5300.cpp
@@ -10,7 +10,22 @@
 #include "db_cxx.h"
 #include "SQLParser.h"
 
+using namespace std;
+using namespace hsql;
+
 int main(int argc, char *argv[]) {
-	cout << "hello!";
+	//Read user input in a loop
+	while (true) {
+		cout << "SQL> ";
+		string q;
+		getline(cin, q);
+		if (q.length() == 0)
+			continue;
+		if (q == "quit")
+			break;
+
+		cout << "Your query was: " << q << endl;
+	}	
+	
 }	
 
