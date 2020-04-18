c47e09ba4d4f54dfb77593c63ffaa1f7a6b9b584
Matthew Echert <mgechert@gmail.com>
2020-04-18 13:58:29 -0700

    Created Milestone1 project files

---
+++ sql5300.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file sql5300.cpp - main entry for the relation manager SQL shell
+ * @author Yi Niu, Matthew Echert
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "db_cxx.h"
+#include "SQLParser.h"
+
+int main(int argc, char *argv[]) {
+	cout << "hello!";
+}	
+
